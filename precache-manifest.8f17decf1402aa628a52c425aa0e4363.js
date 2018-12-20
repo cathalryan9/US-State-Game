b967b1e46d1eb0380f499b8eaebac20f52fe1e57
Cathal Ryan <cathalryan92@gmail.com>
2018-12-20 12:41:19 +0000

    Updates

---
+++ precache-manifest.8f17decf1402aa628a52c425aa0e4363.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "272575831107310c6dbb",
+    "url": "/US-State-Game/static/css/main.1c2a2739.chunk.css"
+  },
+  {
+    "revision": "272575831107310c6dbb",
+    "url": "/US-State-Game/static/js/main.27257583.chunk.js"
+  },
+  {
+    "revision": "6ae1e4af6881f6ae305d",
+    "url": "/US-State-Game/static/js/1.6ae1e4af.chunk.js"
+  },
+  {
+    "revision": "62c4daeebf9af19aeeef",
+    "url": "/US-State-Game/static/js/runtime~main.62c4daee.js"
+  },
+  {
+    "revision": "8cc8a3fcc4fea6fc8e6050ac41b91d53",
+    "url": "/US-State-Game/index.html"
+  }
+];
